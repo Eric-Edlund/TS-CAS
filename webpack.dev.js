8fa70d4c46175a5df212990e82b7fa2c795e93f1
Eric Edlund <ericedlund2017@gmail.com>
2023-10-28 22:59:32 -0400

    Exlcude tests from website build

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,11 +4,4 @@ const common = require('./webpack.common.js');
 module.exports = merge (common, {
   mode: 'development',
   devtool: 'inline-source-map',
-  /*
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000,
-  },
-  */
 });
