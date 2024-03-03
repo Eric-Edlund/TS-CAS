94d149cbc95e2a7b8dca8382b081e290a4e102b6
Eric Edlund <ericedlund2017@gmail.com>
2024-03-03 15:48:01 -0500

    Import MathQuill and refator frontend
    I added the latest MathQuill Latex expression editor to the
    project, then redid the frontend so that either MathQuill or
    the existing expression parsing can be used. MathQuill
    isn't yet functional in the project.

--- ui/LoadWasmStepsPage.ts
+++ ui/LoadWasmStepsPage.ts
@@ -7,80 +7,133 @@ import { parseExpressionJSON } from "./mathlib/expressions-from-json"
 declare const MathJax: any
 declare const MQ: any
 
+type InputMode = "quill" | "boring"
+let currentInputMode: InputMode
+
+// Only accessible after the DOM is loaded
+let activeUI: QuillInputUI | LegacyInputUI
+
+type QuillInputUI = {
+    quillInput: any
+}
+
+type LegacyInputUI = {
+    textInput: HTMLTextAreaElement
+    inputView: EditableMathView
+}
+
+const answerSummary = document.getElementById(
+    "answerSummary"
+)! as HTMLDivElement
+
+// Displays the final answer
+const solutionView = new EditableMathView()
+
+// Displays sequence of steps leading to the final answer
+const stepListView = document.getElementById("stepsView")! as HTMLDivElement
+
+/**
+ * Called after the DOM is loaded.
+ */
 export async function loadWasmStepsBackend(): Promise<void> {
     await initWasm()
 
-    // Where the text input goes
-    const inputView = document.getElementById(
-        "inputView"
-    )! as HTMLTextAreaElement
-
-//     const _quil = document.getElementById("mathquill");
-//     let _config = {
-//         handlers: { edit: function(){} },
-//         restrictMismatchedBrackets: true
-//     };
-//     const input = MQ.MathField(_quil, _config)
-//     console.debug(input)
-
-    // Displays the typeset input
-    const problemView = new EditableMathView()
-    {
-        const problemViewDiv = document.getElementById(
-            "problemView"
-        ) as HTMLDivElement
-        problemViewDiv.appendChild(problemView)
-    }
-    // Displays the final answer
-    const solutionView = new EditableMathView()
-    {
-        const solutionViewDiv = document.getElementById("solutionView")!
-        solutionViewDiv.appendChild(solutionView)
-    }
-    // Displays sequence of steps
-    const stepListView = document.getElementById("stepsView")! as HTMLDivElement
-
-    // Populate ui
-
-    inputView.focus()
-    let parsedExpression: Expression
-
-    inputView.addEventListener("input", () => {
-        parsedExpression = parseExpression(inputView.value) ?? parsedExpression
-        if (parsedExpression == undefined) return
-
-        problemView.value = parsedExpression
-
-        let r = simplify_with_steps(parsedExpression.toJSON())
-
-        let result: {
-            steps: string[]
-        }
-        try {
-            result = JSON.parse(r)
-        } catch (e) {
-            console.log(
-                "Implementation error: Received error msg from backend:"
-            )
-            console.log(r)
-            return
-        }
-
-        stepListView.innerHTML = ""
-        for (let i = 1; i + 1 < result.steps.length; i += 2) {
-            let argument = result.steps[i]
-            let expression = result.steps[i + 1]
-
-            stepListView.appendChild(row(argument, expression))
-        }
-
-        solutionView.value = parseExpressionJSON(
-            result.steps[result.steps.length - 1]
-        )
-        MathJax.typeset([problemView, stepListView])
-    })
+    setInputMode("boring")
 }
 
+// The last valid entered expression
+let expression: Expression
+
+/**
+ * Calculates the new answer and displays it.
+ * @effects The solution steps view and summary div.
+ *      Does not effect the input area.
+ */
+function onInputExpressionChanged() {
+    if (expression == undefined) return
+
+    let r = simplify_with_steps(expression.toJSON())
+
+    let result: {
+        steps: string[]
+    }
+    try {
+        result = JSON.parse(r)
+    } catch (e) {
+        console.log("Implementation error: Received error msg from backend:")
+        console.log(r)
+        return
+    }
+
+    solutionView.value = parseExpressionJSON(
+        result.steps[result.steps.length - 1]
+    )
+    stepListView.innerHTML = ""
+    for (let i = 1; i + 1 < result.steps.length; i += 2) {
+        let argument = result.steps[i]
+        let expression = result.steps[i + 1]
+
+        stepListView.appendChild(row(argument, expression))
+    }
+
+    MathJax.typeset([answerSummary, stepListView])
+}
+
+/**
+ * Makes the chosen current view the visible one.
+ * @effects activeUI, currentInputMode, DOM
+ */
+export function setInputMode(mode: InputMode): void {
+    if (currentInputMode == mode) return
+    currentInputMode = mode
+
+    const inputDiv = document.getElementById("inputZone")!
+
+    if (mode == "quill") {
+        const inputView = document.createElement("span")
+        const quill = MQ.MathField(inputView)
+        activeUI = {
+            quillInput: quill
+        } as QuillInputUI
+
+        inputDiv.replaceChildren(inputView)
+        answerSummary.replaceChildren(solutionView)
+    } else {
+        const inputArea = document.createElement("textarea")
+        const inputView = new EditableMathView()
+        inputView.classList.add("col", "s5")
+        inputArea.addEventListener("input", () => {
+            expression = parseExpression(inputArea.value) ?? expression
+            inputView.value = expression
+            onInputExpressionChanged()
+        })
+
+        activeUI = {
+            textInput: inputArea,
+            inputView: inputView
+        } as LegacyInputUI
+
+        inputDiv.replaceChildren(inputArea)
+
+        answerSummary.replaceChildren()
+        answerSummary.appendChild(inputView)
+        const eqSymbol = document.createElement("div")
+        eqSymbol.classList.add("col", "s2")
+        eqSymbol.innerHTML = "<math display='block'><mo>=</mo></math>"
+        answerSummary.appendChild(eqSymbol)
+        solutionView.classList.add("col", "s5")
+        answerSummary.appendChild(solutionView)
+
+        MathJax.typeset([answerSummary])
+    }
+}
+
+//@ts-ignore
+window.setInputMode = setInputMode
+
+/**
+ * Creates an argument row for the solution steps list.
+ */
 function row(argument: string, expression: string): HTMLDivElement {
     const row = document.createElement("div")
     row.classList.add("row")
