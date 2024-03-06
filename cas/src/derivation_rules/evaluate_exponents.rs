dff0cee561377f0b30b7d7972c944dfa6f565183
Eric Edlund <ericedlund2017@gmail.com>
2024-03-06 17:52:56 -0500

    Add rule to EvaluateExponents

---
+++ cas/src/derivation_rules/evaluate_exponents.rs
@@ -0,0 +1,49 @@
+use std::rc::Rc;
+
+use crate::{argument::Argument, expressions::{Expression, ExpressionPtr, Integer}};
+
+use super::DerivationRule;
+
+
+pub struct EvaluateExponents {}
+
+impl DerivationRule for EvaluateExponents {
+    fn apply(&self, input: ExpressionPtr) -> Vec<(ExpressionPtr, Rc<Argument>)> {
+        let exponent = match input {
+            Expression::Exponent(ref e) => e,
+            _ => return vec![]
+        };
+
+        let Expression::Integer(ref base) = exponent.base()
+        else {
+            return vec![];
+        };
+
+        let Expression::Integer(ref power) = exponent.power()
+        else {
+            return vec![];
+        };
+
+        vec![(
+            Integer::of(base.value().pow(power.value())),
+            Argument::new(String::from("Evaluate exponents"), vec![input])
+        )]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{convenience_expressions::i, expressions::Exponent};
+
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let rule = EvaluateExponents {};
+
+        let start = Exponent::of(i(7), i(8));
+        let result = rule.apply(start).first().unwrap().0.clone();
+
+        assert_eq!(result, i(5764801));
+    }
+}
