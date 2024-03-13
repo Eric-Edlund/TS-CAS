a2c7f36bdfd96f5f39b4630f4380f2885c632f15
Eric Edlund <ericedlund2017@gmail.com>
2024-03-13 13:24:59 -0400

    Pull WASM into Wrapper Crate
    Create a second crate called cas-wasm-wrapper which depends on the cas
    crate. This allows us to publish the cas crate as a pure Rust crate.
    This also resolves a testing issue where integration tests can't be run
    on c-dynamic lib crates. Now integration tests can run on the cas crate
    without me touching config files.
    
    The wrapper crate contains just wrapper functions for the api in cas.
    Updated webpack to use the wrapper crate.

---
+++ cas-wasm-wrapper/src/lib.rs
@@ -0,0 +1,15 @@
+use wasm_bindgen::prelude::*;
+
+/// Just provide wrappers for the functions in cas. This is only a compilation
+/// target.
+
+#[wasm_bindgen]
+pub fn simplify_with_steps(json_expression: &str, search_depth: u32, optimizer: &str) -> String {
+    cas::simplify_with_steps(json_expression, search_depth, optimizer)
+}
+
+#[wasm_bindgen]
+pub fn get_all_equivalents(json_expression: &str, search_depth: u32, optimizer: &str) -> String {
+    cas::get_all_equivalents(json_expression, search_depth, optimizer)
+}
+
--- cas/src/deriver.rs
+++ cas/src/deriver.rs
@@ -3,7 +3,6 @@ use std::rc::Rc;
 
 use petgraph::data::DataMap;
 use petgraph::graph::NodeIndex;
-use wasm_bindgen::prelude::wasm_bindgen;
 
 use crate::argument::Argument;
 use crate::derivation_rules::{ALL_RULES, STRICT_SIMPLIFYING_RULES};
@@ -171,16 +170,6 @@ impl Deriver {
         }
     }
 }
-
-#[wasm_bindgen]
-extern "C" {
-    // Use `js_namespace` here to bind `console.log(..)` instead of just
-    // `log(..)`
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-}
-
-
 
 type Derivation = (Expression, Rc<Argument>);
 type EquivList = Vec<Derivation>;
