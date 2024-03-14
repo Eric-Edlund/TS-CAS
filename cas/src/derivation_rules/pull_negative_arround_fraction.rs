e38f1ad31d0a039e6c512f6b577f65fc4f1dc9c4
Eric Edlund <ericedlund2017@gmail.com>
2024-03-14 17:13:21 -0400

    Add integration test + Rule for pulling negatives out of fraction

---
+++ cas/src/derivation_rules/pull_negative_arround_fraction.rs
@@ -0,0 +1,48 @@
+use std::rc::Rc;
+
+use crate::{argument::Argument, expressions::{Expression, ExpressionPtr, Fraction, Negation}};
+
+use super::DerivationRule;
+
+
+pub struct PullNegativeOutOfFraction {}
+
+impl DerivationRule for PullNegativeOutOfFraction {
+    fn apply(&self, input: ExpressionPtr) -> Vec<(ExpressionPtr, Rc<Argument>)> {
+        let frac = match input {
+            Expression::Fraction(ref f) => f,
+            _ => return vec![]
+        };
+
+        let num = match frac.numerator() {
+            Expression::Negation(n) => Some(n),
+            _ => None
+        };
+        let den = match frac.denominator() {
+            Expression::Negation(n) => Some(n),
+            _ => None
+        };
+
+
+        let result = if num.is_some() && den.is_some() {
+            return vec![];
+        } else if num.is_none() && den.is_none() {
+            return vec![];
+        } else if num.is_some() {
+            Negation::of(Fraction::of(
+                num.unwrap().child(), 
+                frac.denominator()
+            ))
+        } else {
+            Negation::of(Fraction::of(
+                frac.numerator(),
+                den.unwrap().child()
+            ))
+        };
+
+        vec![(
+            result,
+            Argument::new(String::from("Pull negative out of fraction"), vec![input])
+        )]
+    }
+}
