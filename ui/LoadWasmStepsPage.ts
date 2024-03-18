6ef4e239b8063f10dbab48edd3afb08c05c4ec12
Eric Edlund <ericedlund2017@gmail.com>
2024-03-18 17:46:59 -0400

    Remove option for ascii math input

--- ui/LoadWasmStepsPage.ts
+++ ui/LoadWasmStepsPage.ts
@@ -1,27 +1,11 @@
 import { EditableMathView } from "./mathlib/uielements/EditableMathView"
 import initWasm, { get_all_equivalents, simplify_with_steps } from "../cas-wasm-wrapper/pkg"
-import { parseExpression } from "./mathlib/userinput/AntlrMathParser"
 import { Expression } from "./mathlib/expressions/Expression"
 import { parseExpressionJSON } from "./mathlib/expressions-from-json"
 import { parseExpressionLatex } from "./mathlib/userinput/LatexParser"
 
 declare const MathJax: any
 declare const MQ: any
-
-type InputMode = "quill" | "boring"
-let currentInputMode: InputMode
-
-// Only accessible after the DOM is loaded
-let activeUI: QuillInputUI | LegacyInputUI
-
-type QuillInputUI = {
-    quillInput: any
-}
-
-type LegacyInputUI = {
-    textInput: HTMLTextAreaElement
-    inputView: EditableMathView
-}
 
 const answerSummary = document.getElementById(
     "answerSummary"
@@ -33,12 +17,33 @@ const solutionView = new EditableMathView()
 // Displays sequence of steps leading to the final answer
 const stepListView = document.getElementById("stepsView")! as HTMLDivElement
 
+const inputView = document.getElementById("input")
+
 /**
  * Called after the DOM is loaded.
  */
 export async function loadWasmStepsBackend(): Promise<void> {
     await initWasm()
-    setInputMode("quill")
+    const quill = MQ.MathField(inputView, {
+        handlers: {
+            edit: function() {
+                expression = parseExpressionLatex(quill.latex())
+                if (expression == null) {
+                    inputView.style.color = "red"
+                    // Also set border color 
+                    // https://docs.mathquill.com/en/latest/Config/#changing-colors
+                    inputView.style.borderColor = "red"
+                } else {
+                    inputView.style.color = "black"
+                    inputView.style.borderColor = "black"
+                }
+                onInputExpressionChanged()
+            }
+        },
+        autoCommands: 'int pi sqrt'
+    })
+
+    answerSummary.replaceChildren(solutionView)
 }
 
 // The last valid entered expression
@@ -97,82 +102,6 @@ function onInputExpressionChanged() {
 
     MathJax.typeset([answerSummary, stepListView])
 }
-
-/**
- * Makes the chosen current view the visible one.
- * @effects activeUI, currentInputMode, DOM
- */
-export function setInputMode(mode: InputMode): void {
-    if (currentInputMode == mode) return
-    currentInputMode = mode
-
-    const inputDiv = document.getElementById("inputZone")!
-
-    if (mode == "quill") {
-        const inputView = document.createElement("span")
-        inputView.classList.add('col', 's12')
-        // inputView.style.minHeight = "2em"
-        inputView.style.fontSize = "2em"
-        inputView.style.textAlign = "center"
-        //inputView.style.text
-        inputView.style.padding = ".5em"
-        const quill = MQ.MathField(inputView, {
-            handlers: {
-                edit: function() {
-                    expression = parseExpressionLatex(quill.latex())
-                    if (expression == null) {
-                        inputView.style.color = "red"
-                        // Also set border color 
-                        // https://docs.mathquill.com/en/latest/Config/#changing-colors
-                        inputView.style.borderColor = "red"
-                    } else {
-                        inputView.style.color = "black"
-                        inputView.style.borderColor = "black"
-                    }
-                    onInputExpressionChanged()
-                }
-            },
-            autoCommands: 'int pi sqrt'
-        })
-        
-        activeUI = {
-            quillInput: quill
-        } as QuillInputUI
-
-        inputDiv.replaceChildren(inputView)
-        answerSummary.replaceChildren(solutionView)
-    } else {
-        const inputArea = document.createElement("textarea")
-        const inputView = new EditableMathView()
-        inputView.classList.add("col", "s5")
-        inputArea.addEventListener("input", () => {
-            expression = parseExpression(inputArea.value) ?? expression
-            inputView.value = expression
-            onInputExpressionChanged()
-        })
-
-        activeUI = {
-            textInput: inputArea,
-            inputView: inputView
-        } as LegacyInputUI
-
-        inputDiv.replaceChildren(inputArea)
-
-        answerSummary.replaceChildren()
-        answerSummary.appendChild(inputView)
-        const eqSymbol = document.createElement("div")
-        eqSymbol.classList.add("col", "s2")
-        eqSymbol.innerHTML = "<math display='block'><mo>=</mo></math>"
-        answerSummary.appendChild(eqSymbol)
-        solutionView.classList.add("col", "s5")
-        answerSummary.appendChild(solutionView)
-
-        MathJax.typeset([answerSummary])
-    }
-}
-
-//@ts-ignore
-window.setInputMode = setInputMode
 
 /**
  * Creates an argument row for the solution steps list.
