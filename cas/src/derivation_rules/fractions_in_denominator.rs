a7ef1438a5b3b2631b5d3f77921098b928eb8eaf
Eric Edlund <ericedlund2017@gmail.com>
2024-04-30 12:16:05 -0400

    Add tests for and fix fraction in denominator rule

--- cas/src/derivation_rules/fractions_in_denominator.rs
+++ cas/src/derivation_rules/fractions_in_denominator.rs
@@ -19,14 +19,10 @@ impl DerivationRule for FractionInDenominator {
             _ => return vec![],
         };
 
-        let Expression::Fraction(ref inner) = outer.denominator() else {
-            return vec![];
-        };
-
         vec![(
             product_of(&[
                 Fraction::of(outer.numerator(), Integer::of(1)),
-                Fraction::of(inner.denominator(), inner.numerator()),
+                Fraction::of(outer.denominator(), Integer::of(1)),
             ]),
             Argument::new(
                 String::from("Multiply out fraction in denominator"),
@@ -36,5 +32,28 @@ impl DerivationRule for FractionInDenominator {
     }
     fn name(&self) -> String {
         String::from("FractionsInDenominator")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::convenience_expressions::{i, v};
+
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let rule = FractionInDenominator {};
+
+        let start = Fraction::of(Fraction::of(v("x"), i(2)), i(2));
+        let result = rule.apply(start).first().unwrap().0.clone();
+
+        assert_eq!(
+            result,
+            product_of(&[
+                Fraction::of(Fraction::of(v("x"), i(2)), Integer::of(1)),
+                Fraction::of(Integer::of(2), Integer::of(1))
+            ])
+        );
     }
 }
