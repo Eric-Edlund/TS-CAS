229e0f856941db54317de93e79f55d544e3172f3
Eric Edlund <ericedlund2017@gmail.com>
2024-04-30 10:48:42 -0400

    Add rule ZeroTimesAnything

---
+++ cas/src/derivation_rules/zero_times_anything.rs
@@ -0,0 +1,29 @@
+use std::rc::Rc;
+
+use crate::{argument::Argument, expressions::Integer, Expression};
+
+use super::DerivationRule;
+
+pub struct ZeroTimesAnything {}
+
+impl DerivationRule for ZeroTimesAnything {
+    fn apply(&self, input: Expression) -> Vec<(Expression, Rc<Argument>)> {
+        let product = match input {
+            Expression::Product(ref p) => p,
+            _ => return vec![],
+        };
+
+        if !product.factors().contains(&Integer::of(0)) {
+            return vec![];
+        }
+
+        vec![(
+            Integer::of(0),
+            Argument::new(String::from("Anything times zero is zero"), vec![input]),
+        )]
+    }
+
+    fn name(&self) -> String {
+        String::from("ZeroAnything")
+    }
+}
