a2660b4610d47b0cfca444966ff66401dc9dfe2d
Eric Edlund <ericedlund2017@gmail.com>
2024-05-03 19:33:13 -0400

    Fix bug in trig expressions rendering inline powers

--- ui/mathlib/expressions/TrigExp.ts
+++ ui/mathlib/expressions/TrigExp.ts
@@ -1,8 +1,11 @@
 import { VariableValueMap } from "../VariableValueMap"
+import { inParen, inRow } from "../util/MathMLHelpers";
 import { Expression } from "./Expression"
 import { FractionType } from "./Fraction";
 import { IntegerType } from "./Integer";
 import { NameTable } from "./MathElement";
+import { ProductType } from "./Product";
+import { SumType } from "./Sum";
 import { VariableType } from "./Variable";
 
 export type TrigFn = "Sin" | "Cos" | "Tan" | "Sec" | "Csc" | "Cot"
@@ -54,17 +57,25 @@ export class TrigExp extends Expression {
                 && exp.class !== VariableType
                 && exp.class !== FractionType
             ) {
-                return '<mo>(</mo>' + exp.toMathXML(table) + '<mo>)</mo>'
+                return inParen(exp.toMathXML(table))
             }
             return exp.toMathXML(table);
+        }
+
+        function rowIfNeeded(exp: Expression): string {
+            let result = exp.toMathXML(table)
+            if (exp.class === SumType || exp.class === ProductType) {
+                result = inRow(result)
+            }
+            return result
         }
 
         return "<mrow>" + 
             (power != null ? "<msup>" : "") + 
             "<mtext>" +
             this.operation.toLowerCase() + 
-            "</mtext>" +
-            (power != null ? `${power.toMathXML(table)}</msup>` : "") + 
+            "</mtext>" + 
+            (power != null ? `${rowIfNeeded(power)}</msup>` : "") + 
             "<mspace width=\"0.5em\"/>" + 
             "<mrow>" + wrapIfNeeded(this.exp) + "</mrow></mrow>"
 
