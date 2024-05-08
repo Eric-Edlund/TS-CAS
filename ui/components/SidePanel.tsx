e8bc7f8e88d84bbe7f46b69ad778f060ba587ae1
Eric Edlund <ericedlund2017@gmail.com>
2024-05-08 15:54:01 -0400

    Improve linkedin section, add licenses

---
+++ ui/components/SidePanel.tsx
@@ -0,0 +1,28 @@
+
+export function SidePanel() {
+    return <div>
+        <EmployMe/>
+        <div class="container">
+            <OpenSourceLicenses/>
+            <p><a style="color: black;" target="_blank" href="https://github.com/Eric-Edlund/TS-CAS">Project Respository</a></p>
+        </div>
+    </div>
+}
+
+function EmployMe() {
+    return <div class="green light-1 row white-text" style="padding: 0.4em">
+        <img class="responsive-img circle col s4" src="Eric.jpg"></img>
+        <div class="col s8 row">
+            <h5>Employ me!</h5>
+            <p>
+                <a target="_blank" style="color: white" href="https://www.linkedin.com/in/eric-edlund">LinkedIn </a>
+                |
+                <a target="_blank" style="color: white" href="https://github.com/Eric-Edlund"> GitHub</a>
+            </p>
+        </div>
+    </div>
+}
+
+function OpenSourceLicenses() {
+    return <p><a style="color: black" target="_blank" href="https://github.com/Eric-Edlund/TS-CAS/blob/master/CREDITS.md">Open Source Licenses</a></p>
+}
